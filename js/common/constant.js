82736ee9c7a9b4eb2f43a4a4bb0ef5c97ffe681a
ehtmal <ehtmal@gmail.com>
2022-06-28 11:44:47 +0700

    Add constant and some utility for later use

---
+++ js/common/constant.js
@@ -0,0 +1,13 @@
+const FLAGS_URL = '../images/flags/{countryCode}.svg';
+const ITEMS_COLOR = {
+    confirmed: '#ff416c',
+    active: '#286eff',
+    recovered: '#61ce81',
+    deaths: '#8643e6'
+};
+const MOBILE_RESOLUTION = 991;
+const PLAY_TIME = {
+    X1: 2000,
+    X2: 1000,
+    X4: 500
+}
