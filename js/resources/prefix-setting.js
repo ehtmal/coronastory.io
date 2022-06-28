04bb4003f58af06c2f006106a4d2097cd89e20fd
ehtmal <ehtmal@gmail.com>
2022-06-28 11:37:32 +0700

    Story: add auto story rule and manual story data

---
+++ js/resources/prefix-setting.js
@@ -0,0 +1,13 @@
+var SETTING_DEFAULT = {
+  ruleName: 'default',
+  storyName: 'default',
+  config: {
+    tile: 'https://basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png',
+    item: 'confirmed',
+    radius: [4, 80]
+  },
+  camera: {
+    center: [0, 120],
+    zoom: 2,
+  }
+};
